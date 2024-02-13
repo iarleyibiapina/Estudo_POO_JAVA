2c1baf0449f3a3b8fb126085ae63953731d00cb4
iarley <7iarley7@gmail.com>
2024-02-13 17:30:10 -0300

    Feat: Faz agregação entre classes

---
+++ praticando_exercicio/src/Visualizacao.java
@@ -0,0 +1,20 @@
+public class Visualizacao {
+    private Inscrito espectador;
+    private Video video;
+
+    public void setVideo(Video video) {
+        this.video = video;
+    }
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setEspectador(Inscrito espectador) {
+        this.espectador = espectador;
+    }
+
+    public Inscrito getEspectador() {
+        return espectador;
+    }
+    // sobrepor @avaliar
+}
