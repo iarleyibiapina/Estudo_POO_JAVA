d960a232f7d298b42960440de7ef892b71e6ceb7
iarley <7iarley7@gmail.com>
2024-02-13 18:49:56 -0300

    Feat: Instanciando novas classes de visualizacao

--- praticando_exercicio/src/App.java
+++ praticando_exercicio/src/App.java
@@ -11,6 +11,8 @@ public class App {
         System.out.println(v[1].statusVideo());
         System.out.println(v[2].statusVideo());
 
+        System.out.println("----------------------------------------------------------------");
+
         Inscrito i[] = new Inscrito[3];
         i[0] = new Inscrito("Abelio", "20", 'M');
         i[1] = new Inscrito("Damasca", "21", 'F');
@@ -19,5 +21,16 @@ public class App {
         System.out.println(i[0].getInscrito());
         System.out.println(i[1].getInscrito());
         System.out.println(i[2].getInscrito());
+
+        System.out.println("----------------------------------------------------------------");
+        Visualizacao visu[] = new Visualizacao[3];
+
+        visu[0] = new Visualizacao(i[0], v[0]);
+        visu[1] = new Visualizacao(i[1], v[0]);
+        visu[2] = new Visualizacao(i[1], v[1]);
+        // System.out.println(visu[0].getVideo().like());
+        System.out.println(visu[0].getVisualizacao());
+        System.out.println(visu[1].getVisualizacao());
+        System.out.println(visu[2].getVisualizacao());
     }
 }
