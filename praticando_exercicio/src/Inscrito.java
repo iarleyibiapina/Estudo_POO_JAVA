4dc7a95ca050f1b23d500591d43819e539d60005
iarley <7iarley7@gmail.com>
2024-02-13 18:10:35 -0300

    Feat: Implementa construtor e getStatus Inscrito

--- praticando_exercicio/src/Inscrito.java
+++ praticando_exercicio/src/Inscrito.java
@@ -1,6 +1,13 @@
-public class Inscrito {
+public class Inscrito extends Pessoa{
     private boolean login;
     private int totalVideoAssistido;
+
+    public Inscrito(String nome, String idade, char sexo){
+        // 'puxando' construtor da classe pai/super, no caso Pessoa
+        super(nome, idade, sexo);
+        setLogin(true);
+        setTotalVideoAssistido(0);
+    }
 
     public void setLogin(boolean login) {
         this.login = login;
@@ -20,5 +27,10 @@ public class Inscrito {
         if(getLogin()){
             totalVideoAssistido++;
         }
+    }
+
+    public String getInscrito()
+    {
+        return getNome() + " " + getIdade() + " " + getSexo() + "\n" + getLogin() + " " + getTotalVideoAssistido();
     }
 }
