e644e3a8600310218ee00f7893a61b9db81b9041
iarley <7iarley7@gmail.com>
2024-02-13 18:11:31 -0300

    Feat: Instacia as classes

--- praticando_exercicio/src/App.java
+++ praticando_exercicio/src/App.java
@@ -1,5 +1,23 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Criando varios videos em um array
+        Video v[] = new Video[3];
+        v[0] = new Video("Video Um");
+        v[1] = new Video("Video Dois");
+        v[2] = new Video("Video Tres");
+
+        System.out.println(v[0].statusVideo());
+        System.out.println(v[1].statusVideo());
+        System.out.println(v[2].statusVideo());
+
+        Inscrito i[] = new Inscrito[3];
+        i[0] = new Inscrito("Abelio", "20", 'M');
+        i[1] = new Inscrito("Damasca", "21", 'F');
+        i[2] = new Inscrito("Kaum", "22", 'M');
+
+        System.out.println(i[0].getInscrito());
+        System.out.println(i[1].getInscrito());
+        System.out.println(i[2].getInscrito());
     }
 }
